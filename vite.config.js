5331430ac894d4594bb1314daaf36921a6f8cdf9
crowelian-w <harricgi@gmail.com>
2023-03-01 15:13:02 +0200

    configure vite to use .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    experimental: {
+      renderBuiltUrl(filename) {
+        if (mode === "development") return;
+        // here we set the base url. You might have to change this for react:
+        if (env.VITE_APP_URL == undefined) return filename;
+        return env.VITE_APP_URL + "/" + filename; // THIS fixes the wrong url in index.html after build.
+      },
+    },
+  };
+});
